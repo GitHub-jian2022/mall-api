1f0921e3d023aba3877238a7686b3cb3920d5cd7
GitHub-jian2022 <ianj15@163.com>
2020-11-24 11:57:11 +0800

    'sql'

--- config/sql.js
+++ config/sql.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  // host: '127.0.0.1',
-  host: '47.100.138.242',
+  host: '127.0.0.1',
   user: 'root',
-  // password: 'root',
-  password: 'Abcd1234.',
+  password: 'root',
   database: 'mall',
   multipleStatements: true // 开启同时执行多条SQL
 });
