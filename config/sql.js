27f778148a06ee86e538061177009f6cad7f5dee
GitHub-jian2022 <ianj15@163.com>
2020-11-26 09:58:22 +0800

    gitignore

--- config/sql.js
+++ config/sql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  // host: '127.0.0.1',
-  host: '47.100.138.242',
+  host: '127.0.0.1',
+  // host: '47.100.138.242',
   user: 'root',
-  // password: 'root',
-  password: 'Abcd1234.',
+  password: 'root',
+  // password: 'Abcd1234.',
   database: 'mall',
   multipleStatements: true // 开启同时执行多条SQL
 });
